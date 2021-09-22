ee21f378e9107bc089e74fe1229e346bfdfc646d
Max <enriquelongton@gmail.com>
2021-09-22 14:40:06 -0400

    fun with components, props, working on edit and delete operations now

---
+++ vue/src/components/Post.vue
@@ -0,0 +1,12 @@
+<template>
+  <card class="w-24rem mt-5 max-w-full">
+    <template #content>
+      <span class="text-white">{{ text }}</span>
+    </template>
+  </card>
+</template>
+
+<script setup lang="ts">
+import Card from "primevue/card";
+defineProps<{ text: string }>();
+</script>
--- vue/src/views/Dashboard.vue
+++ vue/src/views/Dashboard.vue
@@ -1,84 +1,18 @@
 <template>
   <div>Logged In</div>
-  <h1>All Posts</h1>
+
+  <h1>All Users</h1>
   <ul>
-    <li v-for="post in data?.posts" v-bind:key="post.id">
-      <span>{{ post.text }}</span>
+    <li v-for="user in data?.auth_users" v-bind:key="user.id">
+      {{ user.first_name }} {{ user.last_name }}
+      <span class="p-text-secondary">({{ user.email }})</span>
     </li>
   </ul>
-
-  <form @submit.prevent="addPost" class="px-4">
-    <card class="w-24rem mt-5 max-w-full">
-      <template #title>Add Post</template>
-      <!-- <template #subtitle>Fill form to add post</template> -->
-      <template #content>
-        <div class="text-left p-fluid p-grid p-formgrid">
-          <span class="p-float-label mb-4">
-            <input-text
-              type="text"
-              id="text"
-              v-model="formValues.text"
-              required
-            />
-            <label for="text">Post Text</label>
-          </span>
-          <span class="p-float-label mb-4">
-            <input-text
-              type="text"
-              id="text"
-              v-model="formValues.image_url"
-              required
-            />
-            <label for="text">Image Url</label>
-          </span>
-        </div>
-      </template>
-      <template #footer>
-        <p-button type="submit" label="submit" />
-      </template>
-    </card>
-  </form>
+  <posts />
 </template>
 
 <script setup lang="ts">
-import { reactive } from "vue";
-import {
-  useInsertPostMutation,
-  usePostsQuery,
-  usePostsStreamSubscription,
-} from "../api";
-import { useToast } from "primevue/usetoast";
-import Card from "primevue/card";
-import InputText from "primevue/inputtext";
-import { useAuth } from "../hooks/auth";
-const toaster = useToast();
-const { user } = useAuth();
-const { data } = usePostsStreamSubscription();
-const { executeMutation } = useInsertPostMutation();
-
-const formValues = reactive({
-  text: "",
-  image_url: "",
-});
-
-async function addPost() {
-  const { data, error } = await executeMutation({
-    object: {
-      text: formValues.text,
-      image_url: formValues.image_url,
-      user_id: user?.value?.id,
-    },
-  });
-
-  if (data) {
-    console.log(data);
-  } else if (error) {
-    toaster.add({
-      summary: error.message,
-      detail: JSON.stringify(error.graphQLErrors),
-      severity: "warn",
-      life: 5000,
-    });
-  }
-}
+import { useUsersStreamSubscription } from "../api";
+import Posts from "@/components/Posts.vue";
+const { data } = useUsersStreamSubscription();
 </script>
